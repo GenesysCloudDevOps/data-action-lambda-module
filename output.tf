1b6b79c39c173b917e470a5b3ee35cf3546b99c5
john.carnell@genesys.com <john.carnell@genesys.com>
2022-03-02 16:42:18 -0500

    NO-JIRA: Adding output names for integration category and name

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "integration_data_action_category" {
+  value = genesyscloud_integration_action.LambdaDataAction.category
+}
+
+output "integration_data_action_name" {
+  value = genesyscloud_integration_action.LambdaDataAction.name
+}
